bb38d41e64712f58a957b92f7af0df4ce484ce7f
paras <parasad612@gmail.com>
2024-03-11 13:49:54 +0100

    character error

--- app/src/main/java/com/androidfactory/simplerick/MainActivity.kt
+++ app/src/main/java/com/androidfactory/simplerick/MainActivity.kt
@@ -3,67 +3,33 @@ package com.androidfactory.simplerick
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.androidfactory.network.KtorClient
-import com.androidfactory.network.TestFile
-import com.androidfactory.network.models.domain.Character
-import com.androidfactory.network.models.domain.CharacterStatus
-import com.androidfactory.simplerick.components.character.CharacterStatusComponent
+import com.androidfactory.simplerick.screens.CharacterDetailsScreen
+import com.androidfactory.simplerick.ui.theme.RickPrimary
 import com.androidfactory.simplerick.ui.theme.SimpleRickTheme
 
 class MainActivity : ComponentActivity() {
+
     private val ktorClient = KtorClient()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var character by remember {
-                mutableStateOf<Character?>(null)
-            }
-
-            LaunchedEffect(key1 = Unit, block = {
-                character = ktorClient.getCharacter(2)
-            })
-
             SimpleRickTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = RickPrimary
                 ) {
-                    Column {
-                        Greeting("Android")
-                        TestFile()
-
-                        Text(text = character?.name ?: "no character")
-                    }
+                    CharacterDetailsScreen(
+                        ktorClient = ktorClient,
+                        characterId = 1
+                    )
                 }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview
-@Composable
-fun CharacterStatusComponentAlive(){
-    CharacterStatusComponent(characterStatus = CharacterStatus.Alive)
 }
